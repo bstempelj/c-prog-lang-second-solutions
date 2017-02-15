f220e6f0d5683f8035f791ad9f11dfdad0107fe4
bstempelj <blaz.stempelj@gmail.com>
2017-02-15 13:00:42 +0100

    added support for vertical histograms in exer-1-13

--- exercise-1-13.c
+++ exercise-1-13.c
@@ -2,38 +2,66 @@
  * Exercise 1-13
  *
  * Write a program to print a histogram of the lengths
- * of words in its input.
+ * of words in its input. It is possible to choose
+ * between vertical and horizontal type histograms.
  */
 #include <stdio.h>
 
 #define SIZE 30
+#define VERT 1
 
 main()
 {
-	int c, i, j, nc, end;
+	int c, i, j, nc, ncmax, end;
 	int nwords[SIZE];
 	
 	for (i = 0; i < SIZE; ++i)
 		nwords[i] = 0;
 	
-	nc = i = 0;
+	ncmax = nc = i = 0;
 	while ( ((c = getchar()) != EOF) && i < SIZE )
 	{
 		++nc;
 		
 		if (c == ' ' || c == '\n' || c == '\t')
 		{
-			nwords[i++] = nc-1;
+			nwords[i++] = --nc;
+			if (nc > ncmax)
+				ncmax = nc;
+
 			nc = 0;
 		}
 	}
 	
-	end = i;
-	for (i = 0; i < end; ++i)
+	end = i;	
+	if (VERT)
 	{
-		printf("%2d: ", i);
-		for (j = 0; j < nwords[i]; ++j)
-			putchar('#');
-		putchar('\n');
-	}	
+
+		while (ncmax > 0)
+		{
+			for (i = 0; i < end; ++i)
+			{
+				if (nwords[i] == ncmax)
+				{
+					putchar('#');
+					--nwords[i];
+				}
+				else
+					putchar(' ');			
+			}
+		
+			putchar('\n');
+			--ncmax;
+		}
+	}
+	else
+	{
+		for (i = 0; i < end; ++i)
+		{
+			for (j = 0; j < nwords[i]; ++j)
+				putchar('#');
+			putchar('\n');
+		}
+	}
+	
 }
