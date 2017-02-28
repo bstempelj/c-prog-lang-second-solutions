88ff1c55abb9def8d0942ee6bc21f94e39c5235b
bstempelj <blaz.stempelj@gmail.com>
2017-02-28 19:45:37 +0100

    changed exercise 23

--- exercise-1-23.c
+++ exercise-1-23.c
@@ -1,49 +1,75 @@
-/******************************************************
-Exercise 1-23: Write a program to remove all comments
-from a C program. Don't forget to handle quoted strings
-and character constants properly. C comments do not nest.
-*******************************************************/
-
+/**
+ * Exercise 1-23
+ *
+ * Write a program to remove all comments from a C program.
+ * Don't forget to handle quoted strings and character
+ * constants properly. C comments do not nest.
+ */
 #include <stdio.h>
 
-int getline(char s[])
-{
-  int c, i;
+#define MAXLINE 1000
 
-  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
-  {
-    s[i] = c;
-  }
-  s[i] = '\0';
-
-  return i;
-}
+int mygetline(char line[], int lim);
+void nocomments(char old[], char new[]);
 
 int main()
 {
-  int i, j, len, found;
-  char line[80];
-  char new_line[80];
+	int len;
+	char line[MAXLINE];
+	char newline[MAXLINE];	// line with no comments
 
-  found = 0;
-  while (len = getline(line))
-  {
-    for (i = j = 0; i < len; i++)
-    {
-      if (line[i] == '/' && line[i + 1] == '*')
-      {
-        found = 1;
-      }
-      else if (line[i - 1] == '*' && line[i] == '/')
-      {
-        found = 0;
-      }
-      else if (found == 0)
-      {
-        new_line[j++] = line[i];
-      }
-    }
-  }
+	printf("> ");
+	while ((len = mygetline(line, MAXLINE)) > 0)
+	{
+		nocomments(line, newline);
+		printf("%s", newline);
+		printf("> ");
+	}
+}
 
-  return 0;
+int mygetline(char s[], int lim)
+{
+	int c, i;
+
+	for (i=0; i<lim-1 && (c=getchar())!='\n'; ++i)
+		s[i] = c;
+	if (c =='\n')
+		s[i++] = c;
+	s[i] = '\0';
+
+	return i;
+}
+
+// suppose that comments are not nested
+// and user does not use */ without first
+// using */
+void nocomments(char old[], char new[])
+{
+	int i, j;
+	int lcom;	// inside line comment
+	int bcom;	// inside block comment
+
+	i = j = 0;
+	lcom = bcom = 0;
+	while (old[i] != '\0')
+	{
+		if (old[i] == '/' && old[i+1] == '/')
+			lcom = 1;
+
+		if (!lcom)
+		{
+			if (old[i] == '/' && old[i+1] == '*')
+				bcom = 1;
+			else if (old[i] == '/' && old[i-1] == '*' && bcom)
+				bcom = 0;
+			else if (!bcom)
+				new[j++] = old[i];
+		}
+
+		++i;
+	}
+
+	if (lcom || bcom)
+		new[j++] = '\n';
+	new[j] = '\0';
 }
